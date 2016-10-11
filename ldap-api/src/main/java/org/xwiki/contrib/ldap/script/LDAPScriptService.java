7b7dfd10af46ac6f677b9a95c3c9a418fda10587
Alexandru Cotiuga <alexandru.cotiuga@xwiki.com>
2016-10-11 17:54:16 +0300

    LDAP-34 : Create boolean checkConnection api

--- ldap-api/src/main/java/org/xwiki/contrib/ldap/script/LDAPScriptService.java
+++ ldap-api/src/main/java/org/xwiki/contrib/ldap/script/LDAPScriptService.java
@@ -25,6 +25,8 @@ import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.context.Execution;
+import org.xwiki.contrib.ldap.XWikiLDAPConnection;
+import org.xwiki.contrib.ldap.XWikiLDAPException;
 import org.xwiki.contrib.ldap.XWikiLDAPUtils;
 import org.xwiki.script.service.ScriptService;
 
@@ -45,6 +47,9 @@ public class LDAPScriptService implements ScriptService
      */
     @Inject
     private Execution execution;
+    
+    @Inject
+    private XWikiLDAPConnection xwikiLdapConnection;
 
     /**
      * @return the XWiki context associated with this execution.
@@ -55,6 +60,24 @@ public class LDAPScriptService implements ScriptService
     }
 
     // API
+    
+    /**
+     * Check the LDAP connection.
+     * 
+     * @param ldapHost the host of the server to connect to.
+     * @param ldapPort the port of the server to connect to.
+     * @param loginDN the user DN to connect to LDAP server.
+     * @param password the password to connect to LDAP server.
+     * @param pathToKeys the path to SSL keystore to use.
+     * @param ssl if true connect using SSL.
+     * @return true if the connection succeed, false otherwise.
+     * @throws XWikiLDAPException error when trying to open connection.
+     */
+    public boolean checkConnection(String ldapHost, int ldapPort, String loginDN, String password, String pathToKeys,
+        boolean ssl) throws XWikiLDAPException
+    {
+        return xwikiLdapConnection.open(ldapHost, ldapPort, loginDN, password, pathToKeys, ssl, getXWikiContext());
+    }
 
     /**
      * @return {@code true} if the currently configured authentication class extends or is an instance of
