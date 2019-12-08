e26122d5d574b1ce7e3fa16e9e491975e4c38e86
Clemens Robbenhaar <c.robbenhaar@green-meadows.de>
2019-12-08 15:52:50 +0100

    [misc] unused import

--- ldap-authenticator/src/main/java/org/xwiki/contrib/ldap/apachedsapi/internal/LdapGroupsCache.java
+++ ldap-authenticator/src/main/java/org/xwiki/contrib/ldap/apachedsapi/internal/LdapGroupsCache.java
@@ -32,7 +32,6 @@ import org.xwiki.cache.config.LRUCacheConfiguration;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.manager.ComponentLifecycleException;
 import org.xwiki.component.phase.Disposable;
-import org.xwiki.contrib.ldap.apachedsapi.XWikiLdapUtils;
 import org.xwiki.contrib.ldap.apachedsapi.XWikiLdapConfig;
 
 /**
